5a12bc0b780bc80fc06c60a09b546421916a6ab4
云深不知处 <luoguixin1234@gmail.com>
2024-01-29 23:01:46 +0800

    更改api

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.yunshen.yunapp"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.yunshen.yunapp"
-        minSdk = 31
-        targetSdk = 33
+        minSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
